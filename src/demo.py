5a307960e6c2b37b05ef0e3646f7aec7f938bcbb
Francisco Maria Calisto <francisco.calisto@tecnico.ulisboa.pt>
2023-04-21 02:09:57 +0100

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -1,5 +1,10 @@
-import csv
-with open('dataset/overview.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+import json
+
+# Open the JSON file and read its contents
+with open('dataset/00c862f7-f081-41a3-85ba-daf14328b0a9.json') as f:
+    data = json.load(f)
+
+# Access the data in the JSON file
+print(data['rawData']['studyId'])
+print(data['rawData']['modality'])
+print(data['rawData']['stacks'])
